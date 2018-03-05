5ab01d2418ab556ade86b74b92052ecef4284abb
Jankymage <36413963+Jankymage@users.noreply.github.com>
2018-03-05 11:40:43 +0100

    Kleine fixen
    Jump Particles laten verwijderen.

--- Assets/Scripts/PlatformMove.cs
+++ Assets/Scripts/PlatformMove.cs
@@ -17,7 +17,7 @@ public class PlatformMove : MonoBehaviour {
 
 		
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
         moveTimer += 1 * Time.deltaTime * moveSpeed;
 
---
+++ Assets/Scripts/PlayerPlatformMount.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformMount : MonoBehaviour {
+
+
+    //WIP
+    //http://johnstejskal.com/wp/how-to-stop-rigidbodies-sliding-and-falling-off-moving-platforms-in-unity3d-and-2d/
+
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Platform")
+        {
+            m_currMovingPlatform = coll.gameObject.transform;
+            transform.SetParent(m_currMovingPlatform);
+        }
+    }
+}
